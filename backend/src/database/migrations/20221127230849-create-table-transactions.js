b2f2fb07a17bbba24879777b5ad41ef6f3cc769d
Lauro Pereira <lauropera01@gmail.com>
2022-11-27 20:15:51 -0300

    feat(backend): create migration Transactions

---
+++ backend/src/database/migrations/20221127230849-create-table-transactions.js
@@ -0,0 +1,39 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Transactions', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      owner_account_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Accounts',
+          key: 'id',
+        },
+      },
+      receiver_account_id: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'Accounts',
+          key: 'id',
+        },
+      },
+      value: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('Transactions');
+  },
+};
