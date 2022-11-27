766f07dabddc675ff928baca9d92eccb14d1ec7f
Lauro Pereira <lauropera01@gmail.com>
2022-11-27 20:03:20 -0300

    feat(backend): create migration Accounts

---
+++ backend/src/database/migrations/20221127190234-create-table-accounts.js
@@ -0,0 +1,23 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Accounts', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      balance: {
+        allowNull: false,
+        type: Sequelize.INTEGER,
+      },
+    });
+  },
+
+  async down(queryInterface) {
+    await queryInterface.dropTable('Accounts');
+  },
+};
