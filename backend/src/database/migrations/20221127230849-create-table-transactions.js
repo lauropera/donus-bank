594a73a273d1c0700653ba76945f7825494a7080
Lauro Pereira <lauropera01@gmail.com>
2022-11-27 20:17:18 -0300

    fix(backend): add "createdAt" in migration Transactions

--- backend/src/database/migrations/20221127230849-create-table-transactions.js
+++ backend/src/database/migrations/20221127230849-create-table-transactions.js
@@ -30,6 +30,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.INTEGER,
       },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+      },
     });
   },
 
