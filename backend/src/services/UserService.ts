d631700e499feafba3eda95f300ab6f0fa7dd0af
Lauro Pereira <lauropera01@gmail.com>
2022-11-27 22:35:39 -0300

    feat(backend): UserService login

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -1,10 +1,30 @@
+import { compare } from 'bcryptjs';
+import HttpException from '../utils/HttpException';
 import { ILogin } from '../interfaces';
 import User from '../database/models/User';
+import { loginSchema } from './utils/schemas';
 
 class UserService {
-  private _model: User;
+  private _model = User;
+  private _tokenUtils = Token;
 
-  async login(credentials: ILogin): Promise<string> {}
+  private static validateLoginSchema(credentials: ILogin): void {
+    const { error } = loginSchema.validate(credentials);
+    if (error) throw new HttpException(400, 'Campos inválidos');
+  }
+
+  async login(credentials: ILogin): Promise<string> {
+    UserService.validateLoginSchema(credentials);
+
+    const user = await this._model.findOne({ where: { cpf: credentials.cpf } });
+
+    if (!user || !(await compare(credentials.password, user.password))) {
+      throw new HttpException(401, 'CPF ou senha inválidos');
+    }
+
+    const token = await this._tokenUtils.generate(user);
+    return token;
+  }
 }
 
 export default UserService;
