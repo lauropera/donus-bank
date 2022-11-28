a5c4392bef4efec2cfd1ea710c502e3af6a38f02
Lauro Pereira <lauropera01@gmail.com>
2022-11-27 23:54:09 -0300

    refactor(backend): credentials validator

--- backend/src/services/UserService.ts
+++ backend/src/services/UserService.ts
@@ -1,6 +1,7 @@
 import { compare } from 'bcryptjs';
+import { Schema } from 'joi';
 import HttpException from '../utils/HttpException';
-import { ILogin } from '../interfaces';
+import { ILogin, IRegister } from '../interfaces';
 import User from '../database/models/User';
 import { loginSchema } from './utils/validations/schemas';
 import Token from './utils/TokenUtils';
@@ -9,13 +10,16 @@ class UserService {
   private _model = User;
   private _tokenUtils = Token;
 
-  private static validateLoginSchema(credentials: ILogin): void {
-    const { error } = loginSchema.validate(credentials);
-    if (error) throw new HttpException(400, 'Campos inválidos');
+  private static validateCredentialsSchema(
+    schema: Schema,
+    credentials: ILogin | IRegister,
+  ): void {
+    const { error } = schema.validate(credentials);
+    if (error) throw new HttpException(400, error.message);
   }
 
   async login(credentials: ILogin): Promise<string> {
-    UserService.validateLoginSchema(credentials);
+    UserService.validateCredentialsSchema(loginSchema, credentials);
 
     const user = await this._model.findOne({ where: { cpf: credentials.cpf } });
 
