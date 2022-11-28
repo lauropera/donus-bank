d759eef7a753870a103bcce2329a8ae53a4cc5c6
Lauro Pereira <lauropera01@gmail.com>
2022-11-27 23:40:29 -0300

    fix(backend): seeder

--- backend/src/database/seeders/20221128014919-accounts.js
+++ backend/src/database/seeders/20221128014919-accounts.js
@@ -14,6 +14,6 @@ module.exports = {
   },
 
   async down(queryInterface) {
-    await queryInterface.bulkDelete('Users', null, {});
+    await queryInterface.bulkDelete('Accounts', null, {});
   },
 };
