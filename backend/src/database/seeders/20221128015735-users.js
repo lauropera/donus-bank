cf790f51586e4cc9316c3bdc5b4790a31a8e0441
Lauro Pereira <lauropera01@gmail.com>
2022-11-27 23:39:42 -0300

    fix(backend): seeders cpf

--- backend/src/database/seeders/20221128015735-users.js
+++ backend/src/database/seeders/20221128015735-users.js
@@ -6,7 +6,7 @@ module.exports = {
     await queryInterface.bulkInsert('Users', [
       {
         name: 'Arezu',
-        cpf: '123.456.789-10',
+        cpf: '71118137663',
         password:
           '$2a$08$ApIRb.TBtlq5.aVC/jJOFOSTySPw5MbpvsjibNoE6pg0teN.y0Dq2',
         // senha: pokepass
@@ -14,7 +14,7 @@ module.exports = {
       },
       {
         name: 'Red',
-        cpf: '987.654.321-10',
+        cpf: '49798060318',
         password:
           '$2a$08$7yX3jde42BQGNour2LAWjOG3hBpr7x27zWm8j8fTPZeIxmSofoRSC',
         // senha: pokepass123
