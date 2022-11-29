6e407c0104a70c04de41ed9804bd7a0a4cbc82c9
Lauro Pereira <lauropera01@gmail.com>
2022-11-29 14:00:56 -0300

    feat(frontend): environment variable api url

--- frontend/src/hooks/useApi.js
+++ frontend/src/hooks/useApi.js
@@ -1,12 +1,16 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+const api = axios.create({
+  baseURL: process.env.API_URL || 'http://localhost:3001'
+});
+
 export function useApi(url) {
   const [data, setData] = useState([]);
   const [isFetching, setIsFetching] = useState(true);
 
   useEffect(() => {
-    axios
+    api
       .get(url)
       .then((response) => {
         setData(response.data);
