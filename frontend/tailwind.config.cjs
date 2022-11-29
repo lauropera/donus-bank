8d6e6f32ef6d159ac61b3f3c987f0d4228372016
Lauro Pereira <lauropera01@gmail.com>
2022-11-29 13:12:37 -0300

    chore(frontend): tailwind setup

--- frontend/tailwind.config.cjs
+++ frontend/tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*{js,jsx}"],
+  content: ['.index.html','./src/**/*.{jsx,js}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
