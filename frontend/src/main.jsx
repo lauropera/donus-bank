b88329665140ca601f581bf2dad935227f4563b0
Lauro Pereira <lauropera01@gmail.com>
2022-11-29 15:06:01 -0300

    feat(frontend): AuthProvider and AuthContext

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './styles/tailwind.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { AuthProvider } from './context/AuthProvider';
+import './styles/tailwind.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+      <App />
+    </AuthProvider>
   </React.StrictMode>
-)
+);
