4c8f1439c5479c56b8617451b92f2df968686c57
Lauro Pereira <lauropera01@gmail.com>
2022-11-30 13:47:35 -0300

    feat(backend): transactions type migration

---
+++ backend/src/database/migrations/20221130164513-create-table-transactions-type.js
@@ -0,0 +1,24 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('TransactionsType', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        allowNull: false,
+        unique: true,
+        type: Sequelize.STRING,
+      },
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('TransactionsType');
+  },
+};
